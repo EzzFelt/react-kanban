4e74cdf26ba7431315a1ca55c4f10f1f81074f19
Enzo Feltrin <enzobrexo@gmail.com>
2025-04-09 19:39:39 -0300

    Englobando o Tasks Provider no App

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@
 import { Box, Flex, Heading } from "@radix-ui/themes"
 import { CreateTaskForm } from "./components/CreateTaskForm"
 import { TaskBoard } from "./components/TaskBoard"
+import { TasksContextProvider } from "./contexts/taskContext"
 
 function App() {
   return (
     <>
+    <TasksContextProvider>
       <Box maxWidth="80rem" mx="auto">
       <Box height="4rem">
         <Flex align="center" gap="4" height="100%">
@@ -19,6 +21,7 @@ function App() {
         <TaskBoard/>
       </Box>
     </Box>
+    </TasksContextProvider>
     </>
   )
 }
